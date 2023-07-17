b72d264c9f5afcd07634c428d4d205bbf2038507
Dionisius Karel P <dionkarel@gmail.com>
2023-07-17 20:23:36 +0700

    Initial commit - Membuat tampilan untuk menu Home, Login dan Register

---
+++ app/src/main/java/com/example/mystoryapps/ui/LoginActivity.kt
@@ -0,0 +1,55 @@
+package com.example.mystoryapps.ui
+
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.example.mystoryapps.R
+import com.example.mystoryapps.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    private var _binding: ActivityLoginBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.hide()
+
+        setAnimation()
+        setActionForButton()
+    }
+
+    private fun setAnimation() {
+        ObjectAnimator.ofFloat(binding.ivLogoLogin, View.TRANSLATION_X, -30F, 30F).apply {
+            duration = 6000
+            repeatCount = ObjectAnimator.INFINITE
+            repeatMode = ObjectAnimator.REVERSE
+        }.start()
+
+        val tvEmail = ObjectAnimator.ofFloat(binding.tvLoginEmail, View.ALPHA, 1f).setDuration(500)
+        val edtEmail = ObjectAnimator.ofFloat(binding.edtLoginEmail, View.ALPHA, 1f).setDuration(500)
+        val tvPassord = ObjectAnimator.ofFloat(binding.tvLoginPassword, View.ALPHA, 1f).setDuration(500)
+        val edtPswd = ObjectAnimator.ofFloat(binding.edtLoginPassword, View.ALPHA, 1f).setDuration(500)
+        val btLogin = ObjectAnimator.ofFloat(binding.btLogin, View.ALPHA, 1f).setDuration(500)
+
+        AnimatorSet().apply {
+            playSequentially(tvEmail, edtEmail, tvPassord, edtPswd, btLogin)
+            start()
+        }
+    }
+
+    private fun setActionForButton() {
+        binding.btLogin.setOnClickListener {
+            //Login
+        }
+
+        binding.tvRegisterShortcut.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
+    }
+}
---
+++ app/src/main/java/com/example/mystoryapps/ui/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mystoryapps.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mystoryapps.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
